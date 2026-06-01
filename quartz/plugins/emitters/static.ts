e95df0acaa1046cda8c2b112ff93b3fc997cfec4
Pongsakorn Teeraparpwong <paonets@gmail.com>
2026-06-01 21:18:28 +0700

    feat(seo): add canonical URLs and support robots.txt root copy

--- quartz/plugins/emitters/static.ts
+++ quartz/plugins/emitters/static.ts
@@ -13,7 +13,12 @@ export const Static: QuartzEmitterPlugin = () => ({
     await fs.promises.mkdir(outputStaticPath, { recursive: true })
     for (const fp of fps) {
       const src = joinSegments(staticPath, fp) as FilePath
-      const dest = joinSegments(outputStaticPath, fp) as FilePath
+      let dest: FilePath
+      if (fp.startsWith("root/")) {
+        dest = joinSegments(argv.output, fp.slice(5)) as FilePath
+      } else {
+        dest = joinSegments(outputStaticPath, fp) as FilePath
+      }
       await fs.promises.mkdir(dirname(dest), { recursive: true })
       await fs.promises.copyFile(src, dest)
       yield dest
