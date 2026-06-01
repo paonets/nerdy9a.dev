452b0c792910cbc8d5ec4af7fa345cedbc576bd0
Pongsakorn Teeraparpwong <paonets@gmail.com>
2026-06-02 06:02:28 +0700

    Move custom content-page plugin to custom-plugins/ content-page to track customizations in Git

---
+++ custom-plugins/content-page/dist/components/index.d.ts
@@ -0,0 +1,6 @@
+import { QuartzComponent } from "@quartz-community/types";
+
+interface ContentBodyOptions {}
+declare const _default: () => QuartzComponent;
+
+export { _default as ContentBody, type ContentBodyOptions };
---
+++ custom-plugins/content-page/dist/types.d.ts
@@ -0,0 +1,13 @@
+export {
+  BuildCtx,
+  PageGenerator,
+  PageMatcher,
+  ProcessedContent,
+  QuartzPageTypePlugin,
+  QuartzPageTypePluginInstance,
+  QuartzPluginData,
+  StaticResources,
+  VirtualPage,
+} from "@quartz-community/types";
+export { ContentPageOptions } from "./index.js";
+export { ContentBodyOptions } from "./components/index.js";
